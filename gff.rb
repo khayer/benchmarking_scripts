b5792195ed1b8c17746f2c2cc524bd09c69f5886
Katharina Hayer <katharinaehayer@gmail.com>
2013-05-24 15:40:33 -0400

    created gff class

--- gff.rb
+++ gff.rb
@@ -10,16 +10,31 @@ class GFF
   def create_index()
     raise "#{@filename} is already indexed" unless @index == {}
     k = File.open(@filename)
-
     k.each do |line|
       line.chomp!
       next unless line =~ /gene/
-      puts k.pos
+      fields = line.split("\t")
+      id = fields[-1].split("=")[1].split(";")[0]
+      @index[[fields[0],fields[3].to_i,id]] = k.pos
     end
+    k.close
   end
 
-  def multiply(y)
-    @x * y
+  def transcript(chr,pos,id)
+    transcript = []
+    pos_in_file = @index[[chr,pos,id]]
+    k = File.open(@filename)
+    k.pos = pos_in_file
+    k.each do |line|
+      break if line =~ /###/ or line =~ /CDS/
+      next if line =~ /mRNA/
+      line.chomp!
+      fields = line.split("\t")
+      transcript << fields[3].to_i
+      transcript << fields[4].to_i
+    end
+    transcript.sort!
+    puts transcript.join(":")
   end
 
 end
--- test/test_gff.rb
+++ test/test_gff.rb
@@ -12,7 +12,17 @@ class TestGFF < Test::Unit::TestCase
   def test_create_index()
     gff_file = GFF.new("test/data/test.gff")
     gff_file.create_index
-    assert_equal(gff_file.index[["chr7",148033816]],39855)
+    assert_equal(gff_file.index[["chr7",148033816,"scaffold1.path1"]],80)
+    assert_raise RuntimeError do
+      gff_file.create_index
+    end
+  end
+
+  def test_transcript()
+    gff_file = GFF.new("test/data/test.gff")
+    gff_file.create_index
+    gff_file.transcript("chr5",136656235,"scaffold15.path1")
+    #assert_equal(gff_file.index[["chr7",148033816]],1929)
   end
 
 end
