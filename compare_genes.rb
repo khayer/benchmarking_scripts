08cae4597b3d6d19f617cbaed96efb58b0fcc72a
khayer <katharinaehayer@gmail.com>
2013-05-26 21:31:20 -0400

    fixed test to compare types

--- compare_genes.rb
+++ compare_genes.rb
@@ -3,8 +3,8 @@ require "./gff.rb"
 
 class CompareGenes
   def initialize(gff_file,geneinfo_file)
-    @gff = GFF.new(gff_file).create_index
-    @geneinfo = GeneInfo.new(geneinfo_file).create_index
+    @gff = GFF.new(gff_file)
+    @geneinfo = GeneInfo.new(geneinfo_file)
   end
 
   attr_accessor :gff, :geneinfo
--- test/test_compare.rb
+++ test/test_compare.rb
@@ -5,8 +5,8 @@ class TestCompare < Test::Unit::TestCase
 
   def test_initialize
     compare_obj = CompareGenes.new("test/data/test.gff","test/data/test_geneinfo.txt")
-    assert_kind_of( GFF.new, compare_obj.gff )
-    assert_kind_of( GeneInfo.new, compare_obj.geneinfo )
+    assert_kind_of( GFF, compare_obj.gff )
+    assert_kind_of( GeneInfo, compare_obj.geneinfo )
   end
 
   def test_statistics()
