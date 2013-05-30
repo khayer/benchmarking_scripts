dba48c0f36c39b912db7db68184f7de7f43928db
Katharina Hayer <katharinaehayer@gmail.com>
2013-05-30 13:34:16 -0400

    added tests for gtf

--- test/test_compare.rb
+++ test/test_compare.rb
@@ -3,19 +3,35 @@ require 'benchmarking_scripts'
 
 class TestCompare < Test::Unit::TestCase
 
-  def test_initialize
-    compare_obj = CompareGenes.new("test/data/test.gff","test/data/test_geneinfo.txt")
-    assert_kind_of( GFF, compare_obj.gff )
+  def test_initialize_GFF
+    compare_obj = CompareGenesGFF.new("test/data/test_geneinfo.txt","test/data/test.gff")
+    assert_kind_of( GFF, compare_obj.compare_file )
     assert_kind_of( GeneInfo, compare_obj.geneinfo )
   end
 
-  def test_statistics_all()
-    compare_obj = CompareGenes.new("test/data/test.gff","test/data/test_geneinfo.txt")
-    compare_obj.gff.create_index()
+  def test_statistics_all_GFF()
+    compare_obj = CompareGenesGFF.new("test/data/test_geneinfo.txt","test/data/test.gff")
+    compare_obj.compare_file.create_index()
     compare_obj.geneinfo.create_index()
     compare_obj.statistics()
     assert_equal(compare_obj.strong_TP,[1, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0])
     assert_equal(compare_obj.weak_TP,[3, 1, 0, 0, 2, 0, 0, 0, 0, 0, 0])
     assert_equal(compare_obj.all_FP,[36, 0, 0, 2, 6, 0, 0, 0, 0, 0, 0])
+  end
+
+  def test_initialize_GTF
+    compare_obj = CompareGenesGTF.new("test/data/test_geneinfo.txt","test/data/test.gtf")
+    assert_kind_of( GTF, compare_obj.compare_file )
+    assert_kind_of( GeneInfo, compare_obj.geneinfo )
+  end
+
+  def test_statistics_all_GTF()
+    compare_obj = CompareGenesGTF.new("test/data/test_geneinfo.txt","test/data/test.gtf")
+    compare_obj.compare_file.create_index()
+    compare_obj.geneinfo.create_index()
+    compare_obj.statistics()
+    assert_equal(compare_obj.strong_TP,[7, 7, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    assert_equal(compare_obj.weak_TP,[735, 735, 0, 0, 0, 0, 0, 0, 0, 0, 0])
+    assert_equal(compare_obj.all_FP,[33, 33, 0, 0, 0, 0, 0, 0, 0, 0, 0])
   end
 end
