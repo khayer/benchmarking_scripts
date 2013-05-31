2937afc63d8e99ee2258ff3a972d5b86fa227f0d
Katharina Hayer <katharinaehayer@gmail.com>
2013-05-31 14:45:18 -0400

    fixed tests

--- lib/benchmarking_scripts/gff.rb
+++ lib/benchmarking_scripts/gff.rb
@@ -11,12 +11,21 @@ class GFF
     raise "#{@filename} is already indexed" unless @index == {}
     logger.info("Creating index for #{@filename}")
     k = File.open(@filename)
+    last_position = 0
+    id = "unknown"
+    fields = []
     k.each do |line|
       line.chomp!
-      next unless line =~ /gene/
-      fields = line.split(" ")
-      id = fields[-1].split("=")[1].split(";")[0]
-      @index[[fields[0],fields[3].to_i-1,id]] = k.pos
+      if line =~ /gene/
+        fields = line.split(" ")
+        id = fields[-1].split("=")[1].split(";")[0]
+        last_position = k.pos
+      end
+      if line =~ /mRNA/
+        coverage = line.split("coverage=")[1].split(";")[0].to_f
+        identity = line.split("identity=")[1].split(";")[0].to_f
+        @index[[fields[0],fields[3].to_i-1,id]] = last_position if identity >= 95.0 && coverage >= 95.0
+      end
     end
     logger.info("Indexing of #{@index.length} transcripts complete")
     k.close
