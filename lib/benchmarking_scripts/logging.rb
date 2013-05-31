cc4f3b81e820a6a8ebb2e05af20d2bc65de7d873
Katharina Hayer <katharinaehayer@gmail.com>
2013-05-31 16:31:58 -0400

    merging finished

--- lib/benchmarking_scripts/logging.rb
+++ lib/benchmarking_scripts/logging.rb
@@ -1,7 +1,7 @@
 module Logging
   # found this on http://stackoverflow.com/questions/917566/ruby-share-logger-instance-among-module-classes
   @out = STDERR
-  @level = Logger::DEBUG
+  @level = Logger::INFO
 
   def logger
     @logger ||= Logging.logger_for(self.class.name)
