b4ca42aa29bdd17254b8e9ca6bbedcc4b20b7fd4
Katharina Hayer <katharinaehayer@gmail.com>
2013-06-05 11:22:54 -0400

    save changes

--- lib/benchmarking_scripts/feature_quantifications.rb
+++ lib/benchmarking_scripts/feature_quantifications.rb
@@ -8,25 +8,27 @@ class FeatureQuantifications
   attr_accessor :filename, :index
 
   def create_index()
-    #raise "#{@filename} is already indexed" unless @index == {}
-    #logger.info("Creating index for #{@filename}")
-    #k = File.open(@filename)
-    #last_position = 0
+    raise "#{@filename} is already indexed" unless @index == {}
+    logger.info("Creating index for #{@filename}")
+    k = File.open(@filename)
+    last_gene = "unknown"
     #id = "unknown"
     #fields = []
-    #k.each do |line|
-    #  line.chomp!
-    #  if line =~ /gene/
-    #    fields = line.split(" ")
-    #    id = fields[-1].split("=")[1].split(";")[0]
-    #    last_position = k.pos
-    #  end
+    k.each do |line|
+      line.chomp!
+      if line =~ /GENE/
+        last_gene = line.split("\t")[0]
+      if line =~ /transcript/
+        fields = line.split(" ")
+        id = fields[-1].split("=")[1].split(";")[0]
+        last_position = k.pos
+      end
     #  if line =~ /mRNA/
     #    coverage = line.split("coverage=")[1].split(";")[0].to_f
     #    identity = line.split("identity=")[1].split(";")[0].to_f
     #    @index[[fields[0],fields[3].to_i-1,id]] = last_position if identity >= 95.0 && coverage >= 95.0
     #  end
-    #end
+    end
     #logger.info("Indexing of #{@index.length} transcripts complete")
     #k.close
   end
