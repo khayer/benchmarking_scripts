e84aeaf351faa30c300551b7ace430cefc4fdbf1
Katharina Hayer <katharinaehayer@gmail.com>
2013-06-13 13:33:53 -0400

    plot updated

--- lib/benchmarking_scripts/compare_genes_fq_gtf.rb
+++ lib/benchmarking_scripts/compare_genes_fq_gtf.rb
@@ -6,6 +6,8 @@ class CompareGenesFQGTF < CompareGenes
     @truth_genefile = FeatureQuantifications.new(feature_quant_file)
     @compare_file = GTF.new(gtf_file)
     @fpkm_values = []
+    @fpkm_values_1_spliceform = []
+    @fpkm_values_else_spliceform = []
     @strong_TP_by_cov = []
     @weak_TP_by_cov = []
     @all_FP_by_cov = []
@@ -28,6 +30,11 @@ class CompareGenesFQGTF < CompareGenes
             fpkm1 = @truth_genefile.coverage[key]
             fpkm2 = @compare_file.coverage[info]
             @fpkm_values << [key,fpkm1,fpkm2]
+            if @truth_genefile.number_of_spliceforms[key] < 1
+              @fpkm_values_else_spliceform << [key,fpkm1,fpkm2]
+            else
+              @fpkm_values_1_spliceform << [key,fpkm1,fpkm2]
+            end
             break
           end
         end
@@ -36,6 +43,7 @@ class CompareGenesFQGTF < CompareGenes
   end
 
   def plot_fpkm(filename)
+    max_value = 0
     Gnuplot.open do |gp|
       Gnuplot::Plot.new( gp ) do |plot|
 
@@ -55,6 +63,64 @@ class CompareGenesFQGTF < CompareGenes
           y << Math.log(pair[2]+1)
         end
         max_value = [x.max,y.max].max
+        plot.xrange "[-0.5:#{max_value}]"
+        plot.yrange "[-0.5:#{max_value}]"
+
+        plot.data = [
+          Gnuplot::DataSet.new( [x, y] ) do |ds|
+            ds.with= "points lc 2"
+            ds.notitle
+          end
+        ]
+      end
+    end
+    Gnuplot.open do |gp|
+      Gnuplot::Plot.new( gp ) do |plot|
+        filename_1 =  filename + "1_spliceform.png"
+        plot.output filename_1
+        plot.terminal 'png'
+        plot.title "FPKM - 1_spliceform"
+        plot.ylabel "cufflinks"
+        plot.xlabel "truth"
+        plot.xtics 'nomirror'
+        plot.ytics 'nomirror'
+        plot.grid 'xtics'
+        plot.grid 'ytics'
+        x = []
+        y = []
+        @fpkm_values_1_spliceform.each do |pair|
+          x << Math.log(pair[1]+1)
+          y << Math.log(pair[2]+1)
+        end
+        plot.xrange "[-0.5:#{max_value}]"
+        plot.yrange "[-0.5:#{max_value}]"
+
+        plot.data = [
+          Gnuplot::DataSet.new( [x, y] ) do |ds|
+            ds.with= "points lc 2"
+            ds.notitle
+          end
+        ]
+      end
+    end
+    Gnuplot.open do |gp|
+      Gnuplot::Plot.new( gp ) do |plot|
+        filename_2 = filename + "more_than_1_spliceform.png"
+        plot.output filename_2
+        plot.terminal 'png'
+        plot.title "FPKM - more than 1_spliceform"
+        plot.ylabel "cufflinks"
+        plot.xlabel "truth"
+        plot.xtics 'nomirror'
+        plot.ytics 'nomirror'
+        plot.grid 'xtics'
+        plot.grid 'ytics'
+        x = []
+        y = []
+        @fpkm_values_else_spliceform.each do |pair|
+          x << Math.log(pair[1]+1)
+          y << Math.log(pair[2]+1)
+        end
         plot.xrange "[-0.5:#{max_value}]"
         plot.yrange "[-0.5:#{max_value}]"
 
