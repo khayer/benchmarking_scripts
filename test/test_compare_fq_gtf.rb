034a26520fc759be27b1ada24f5efd1bd287bdbd
Katharina Hayer <katharinaehayer@gmail.com>
2013-06-14 11:31:53 -0400

    hitting the pause button

--- test/test_compare_fq_gtf.rb
+++ test/test_compare_fq_gtf.rb
@@ -13,7 +13,7 @@ class TestCompareFQGTF < Test::Unit::TestCase
     compare_obj.compare_file.create_index()
     compare_obj.compare_file.calculate_coverage()
     compare_obj.truth_genefile.create_index()
-    compare_obj.truth_genefile.calculate_coverage(50)
+    compare_obj.truth_genefile.calculate_coverage(123.597457)
     compare_obj.truth_genefile.find_number_of_spliceforms()
     compare_obj.truth_genefile.determine_false_negatives()
     compare_obj.statistics()
@@ -21,10 +21,10 @@ class TestCompareFQGTF < Test::Unit::TestCase
     assert_equal(compare_obj.weak_TP,[567, 320, 147, 57, 21, 11, 3, 4, 0, 4, 0])
     assert_equal(compare_obj.all_FP,[328, 130, 46, 6, 1, 0, 0, 4, 0, 0, 0])
     assert_equal(compare_obj.false_negatives,[65, 23, 16, 12, 8, 0, 2, 0, 0, 4, 0])
-    assert_equal(compare_obj.strong_TP_by_cov,[72, 69, 67, 66, 48, 17, 8, 2])
-    assert_equal(compare_obj.weak_TP_by_cov,[567, 524, 430, 253, 108, 33, 15, 2])
-    assert_equal(compare_obj.all_FP_by_cov,[187, 106, 85, 51, 24])
-    assert_equal(compare_obj.false_negatives_by_cov,[65, 65, 64, 44, 14, 5, 1])
+    assert_equal(compare_obj.strong_TP_by_cov,[72, 67, 66, 50, 21, 8, 2])
+    assert_equal(compare_obj.weak_TP_by_cov,[567, 438, 267, 121, 38, 16, 3])
+    assert_equal(compare_obj.all_FP_by_cov,[187, 89, 51, 26, 7])
+    assert_equal(compare_obj.false_negatives_by_cov,[65, 65, 44, 17, 5, 1])
   end
 
   def test_statistics_plot()
@@ -32,11 +32,11 @@ class TestCompareFQGTF < Test::Unit::TestCase
     compare_obj.compare_file.create_index()
     compare_obj.compare_file.calculate_coverage()
     compare_obj.truth_genefile.create_index()
-    compare_obj.truth_genefile.calculate_coverage(50)
+    compare_obj.truth_genefile.calculate_coverage(123.597457)
     compare_obj.truth_genefile.find_number_of_spliceforms()
     compare_obj.statistics_fpkm()
     compare_obj.plot_fpkm("test/data/test.png")
-    assert_equal(File.size("test/data/test.png"),5736)
+    assert_equal(File.size("test/data/test.png"),5805)
   end
 
   def test_statistics_plot_with_M()
@@ -49,6 +49,6 @@ class TestCompareFQGTF < Test::Unit::TestCase
     compare_obj.truth_genefile.calculate_coverage()
     compare_obj.statistics_fpkm()
     compare_obj.plot_fpkm("test/data/test2.png")
-    assert_equal(File.size("test/data/test2.png"),4871)
+    assert_equal(File.size("test/data/test2.png"),4739)
   end
 end
