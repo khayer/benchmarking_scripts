50a5d6cd8f3625e7277af1fe4c47072f60614fcf
khayer <katharinaehayer@gmail.com>
2013-06-21 12:17:29 -0400

    hitting the pause button

--- test/test_compare_fq_gtf.rb
+++ test/test_compare_fq_gtf.rb
@@ -22,9 +22,9 @@ class TestCompareFQGTF < Test::Unit::TestCase
     assert_equal(compare_obj.all_FP,[328, 130, 46, 6, 1, 0, 0, 4, 0, 0, 0])
     assert_equal(compare_obj.false_negatives,[65, 23, 16, 12, 8, 0, 2, 0, 0, 4, 0])
     assert_equal(compare_obj.strong_TP_by_cov,[72, 67, 59, 34, 13, 6, 1])
-    assert_equal(compare_obj.weak_TP_by_cov,[567, 438, 267, 121, 38, 16, 3])
-    assert_equal(compare_obj.all_FP_by_cov,[187, 89, 51, 26, 7])
-    assert_equal(compare_obj.false_negatives_by_cov,[65, 65, 44, 17, 5, 1])
+    assert_equal(compare_obj.weak_TP_by_cov,[567, 368, 189, 67, 22, 10, 1])
+    assert_equal(compare_obj.all_FP_by_cov,[187, 70, 34, 15])
+    assert_equal(compare_obj.false_negatives_by_cov,[65, 58, 34, 10, 1])
   end
 
   def test_statistics_plot()
@@ -36,7 +36,7 @@ class TestCompareFQGTF < Test::Unit::TestCase
     compare_obj.truth_genefile.find_number_of_spliceforms()
     compare_obj.statistics_fpkm()
     compare_obj.plot_fpkm("test/data/test.png")
-    assert_equal(File.size("test/data/test.png"),5805)
+    assert_equal(File.size("test/data/test.png"),5736)
   end
 
   def test_statistics_plot_with_M()
