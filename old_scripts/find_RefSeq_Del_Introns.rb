d1cce0a409228117746bca2cfd99cb17daf716c0
Katharina Hayer <katharinaehayer@gmail.com>
2013-12-13 15:43:29 -0500

    hitting the pause button

--- old_scripts/find_RefSeq_Del_Introns.rb
+++ old_scripts/find_RefSeq_Del_Introns.rb
@@ -205,7 +205,7 @@ def read_gene_info_file(config_file,fasta)
         sequence = fasta_file[fai_index[chr][:start]..fai_index[chr][:stop]].delete("\n")[exon_stops[i]...exon_starts[i+1]]
         #puts sequence
         splice_signal_num = -1
-        if sequence.length >= 4
+        #if sequence.length >= 4
           if strand == "-"
             #reverse case
             splice_signal_num_donor = $donor_rev.index(sequence[-2..-1])
@@ -214,7 +214,7 @@ def read_gene_info_file(config_file,fasta)
             splice_signal_num_donor = $donor.index(sequence[0..1])
             splice_signal_num_acceptor = $acceptor.index(sequence[-2..-1])
           end
-        end
+        #end
         if splice_signal_num_donor == splice_signal_num_acceptor && splice_signal_num_acceptor
           splice_signal_num = splice_signal_num_acceptor
         end
