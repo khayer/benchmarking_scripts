a5d82b519127bc50704eaee0096d259f469f3fc6
Katharina Hayer <katharinaehayer@gmail.com>
2013-12-16 11:34:54 -0500

    hitting the pause button

--- old_scripts/find_RefSeq_Del_Introns.rb
+++ old_scripts/find_RefSeq_Del_Introns.rb
@@ -244,8 +244,8 @@ def read_gene_info_file(config_file,fasta)
         $logger.debug(splice_signal_num)
         if splice_signal_num == -1
           puts "#{name}\t#{exon_stops[i]}\t#{exon_starts[i+1]}\t#{sequence}\t#{strand}\t#{splice_signal_num}"
-          novel_introns[[chr,exon_stops[i],exon_starts[i+1]]] ||= 0
-          novel_introns[[chr,exon_stops[i],exon_starts[i+1]]] += 0
+          introns_novel[[chr,exon_stops[i],exon_starts[i+1]]] ||= 0
+          introns_novel[[chr,exon_stops[i],exon_starts[i+1]]] += 0
         end
         all_introns[[chr,exon_stops[i],exon_starts[i+1]]] ||= 0
         all_introns[[chr,exon_stops[i],exon_starts[i+1]]] += 1
