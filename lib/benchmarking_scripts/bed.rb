2e2efecf19483225c20e1286674755ca23b7b8c9
Katharina Hayer <katharinaehayer@gmail.com>
2013-12-18 11:24:26 -0500

    Exclude with patterns since GENE.90 and GENE.90.1 needs to be excluded

--- lib/benchmarking_scripts/bed.rb
+++ lib/benchmarking_scripts/bed.rb
@@ -18,7 +18,7 @@ class Bed < FileFormats
       line.chomp!
       fields = line.split("\t")
       id = fields[3]
-      @index[[fields[0],fields[1].to_i,id]] = previous_position unless $EXCLUDE_GENES.include?(id)
+      @index[[fields[0],fields[1].to_i,id]] = previous_position if ($EXCLUDE_GENES.select {|e| id.match(e)}).empty?
       previous_position = @filehandle.pos
     end
     logger.info("Indexing of #{@index.length} transcripts complete")
