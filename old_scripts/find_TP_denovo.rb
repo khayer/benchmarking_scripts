f5110dec3a19fe0072e6e72c5430d3d69903727f
Katharina Hayer <katharinaehayer@gmail.com>
2014-02-06 14:21:20 -0500

    finished find_TP_denovo.rb

--- old_scripts/find_TP_denovo.rb
+++ old_scripts/find_TP_denovo.rb
@@ -1,7 +1,70 @@
 #!/usr/bin/env ruby
+require 'optparse'
+require 'logger'
+require 'spreadsheet'
+require "csv"
+require 'set'
+require "benchmarking_scripts"
+
+# 2014/2/4 Katharina Hayer
+
+$logger = Logger.new(STDERR)
+$truth_sequences = Hash.new()
+$number_of_spliceforms = Hash.new()
+
+# Initialize logger
+def setup_logger(loglevel)
+  case loglevel
+  when "debug"
+    $logger.level = Logger::DEBUG
+  when "warn"
+    $logger.level = Logger::WARN
+  when "info"
+    $logger.level = Logger::INFO
+  else
+    $logger.level = Logger::ERROR
+  end
+end
+
+def setup_options(args)
+  options = {:out_file =>  "STDOUT",:file_format => 'gtf'}
+
+  opt_parser = OptionParser.new do |opts|
+    opts.banner = "Usage: find_TP_cap3 [options] contigs truth_sequences_fa geneinfo.gtf"
+    #opts.banner = "Usage: compare_fpkm_values [options] fpkm_values.txt"
+    opts.separator ""
+
+    opts.on("-o", "--out_file [OUT_FILE]",
+      :REQUIRED,String,
+      "File for the output, Default: STDOUT") do |anno_file|
+      options[:out_file] = anno_file
+    end
+
+    opts.on("-f", "--file_format [FORMAT]",:REQUIRED,String,
+      "File format (Avail: gtf, geneinfo, bed, feature_quant) Default: gtf") do |format|
+      raise "File format #{format} is not availabe!" unless ['gtf', 'geneinfo', 'bed',
+       'feature_quant'].include?(format)
+      options[:file_format] = format
+    end
+
+    opts.on("-v", "--[no-]verbose", "Run verbosely") do |v|
+      options[:log_level] = "info"
+    end
+
+    opts.on("-d", "--debug", "Run in debug mode") do |v|
+      options[:log_level] = "debug"
+    end
+  end
+
+  args = ["-h"] if args.length == 0
+  opt_parser.parse!(args)
+  raise "Please specify the sam files" if args.length == 0
+  options
+end
 
 #### METHODS
 
+=begin
 def search_in(current_sequence)
   result = [0, ""]
   current_sequence_complement = get_reverse_complement(current_sequence)
@@ -35,6 +98,7 @@ def search_in(current_sequence)
       end
     end
   end
+
 
   #if helper
   #ind1 = $truth_sequences.index(current_sequence_complement)
@@ -71,6 +135,7 @@ def search_in(current_sequence)
   #end
   result
 end
+=end
 
 def length_diff(seq1,seq2)
   (seq1.length - seq2.length).abs
@@ -93,155 +158,214 @@ def get_reverse_complement(seq)
   seq_complement
 end
 
-#### MAIN
-
-unless ARGV.length == 2
-  puts ""
-  puts "  Usage:"
-  puts "  find_TP_cap3 contigs truth_sequences_fa"
-  puts ""
-  exit
-end
-
-trinity_file = ARGV[0]
-truth_sequences_file = ARGV[1]
-
-$truth_sequences = Hash.new()
-key = ""
-File.open(truth_sequences_file).each do |line|
-  line.chomp!
-  if line =~ /^>/
-    key = line.split(":")[0].split(">")[1] #>GENE.1.0:chr1:4797973-4836816_+
+def read_anno(geneinfo,file_format)
+  case file_format
+  when "gtf"
+    genes_anno = GTF.new(geneinfo)
+  when "geneinfo"
+    genes_anno = GeneInfo.new(geneinfo)
+  when "feature_quant"
+    genes_anno = FeatureQuantifications.new(geneinfo)
   else
-    $truth_sequences[key] = line
+    raise "File format #{file_format} not supported yet!"
   end
+  genes_anno.find_number_of_spliceforms()
 end
 
-#$expressed_isoforms = []
-#File.open(rsem).each do |line|
-#  next unless line =~ /^comp/
-#  line.chomp!
-#  info = line.split("\t")
-#  iso_pct = info[-1].to_f
-#  $expressed_isoforms << info[0] if iso_pct > 50.00
-#end
-
-
-number_of_transcripts = `grep -c "^>" #{trinity_file}`
-
-current_sequence = ""
-false_positive = 0
-weak_true_positive = 0
-strong_true_positive = 0
-weak_true_positive_by_spliceform = []
-strong_true_positives_by_spliceforms = []
-weak_false_positives_by_spliceforms = []
-
-number_of_all_genes = 0
-current_comp_name = ""
-
-File.open(trinity_file).each do |line|
-
-  line.chomp!
-  if line =~ /^>/ && current_sequence != "" #&& $expressed_isoforms.include?(current_comp_name)
-    number_of_all_genes += 1
-    STDERR.puts "#{( number_of_all_genes.to_f / number_of_transcripts.to_f )*100} %"
-    #STDERR.puts "#{( number_of_all_genes.to_f / 66 ).to_f} %"
-    result = search_in(current_sequence)
-    current_sequence = ""
-    case result[0]
-    when 0
-      #puts "NEGATIVE"
-      false_positive += 1
-      #if result[1] != ""
-      #STDERR.puts result.join(":")
-      if result[1] =~ /GENE/
-        number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
-      else
-        #STDERR.puts "fdsfdgg"
-        number = 0
-      end
-      #puts number
-      weak_false_positives_by_spliceforms[number] = 0 unless weak_false_positives_by_spliceforms[number]
-      weak_false_positives_by_spliceforms[number] += 1
-      #end
-    when 1
-      #puts "WEAK"
-      weak_true_positive += 1
-      number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
-      #puts number
-      weak_true_positive_by_spliceform[number] = 0 unless weak_true_positive_by_spliceform[number]
-      weak_true_positive_by_spliceform[number] += 1
-    when 2
-      #puts "STRONG"
-      strong_true_positive += 1
-      weak_true_positive += 1
-      number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
-      #puts number
-      weak_true_positive_by_spliceform[number] = 0 unless weak_true_positive_by_spliceform[number]
-      weak_true_positive_by_spliceform[number] += 1
-      strong_true_positives_by_spliceforms[number] = 0 unless strong_true_positives_by_spliceforms[number]
-      strong_true_positives_by_spliceforms[number] += 1
+def get_truth_sequences(truth_sequences_file)
+  key = ""
+  File.open(truth_sequences_file).each do |line|
+    line.chomp!
+    if line =~ /^>/
+      key = line.split(":")[0].split(">")[1] #>GENE.1.0:chr1:4797973-4836816_+
+    else
+      $truth_sequences[key] = line.upcase
     end
   end
+end
 
-
-
-  if line =~ /^>/
-    current_comp_name = line.split(" ")[0].split(">")[1]
-    current_sequence = ""
-    next
+def cut_truth_sequences(genes_anno)
+  genes_anno.each_pair do |key, value|
+    transcript = genes_anno.transcript(key)
+    pre_cut_seq = $truth_sequences[key[-1]]
+    seq_length = pre_cut_seq.length
+    start = transcript[1]-transcript[0]-50
+    stop = seq_length-(transcript[-1]-transcript[-2]-50)
+    $truth_sequences[key[-1]] = pre_cut_seq[start..stop]
+    $number_of_spliceforms[key[-1]] = genes_anno.number_of_spliceforms[key]
   end
-  current_sequence += line
 end
 
-result = search_in(current_sequence)
-current_sequence = ""
-case result[0]
-when 0
-  #puts "NEGATIVE"
-  false_positive += 1
-  #if result[1] != ""
-  number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
-  #puts number
-  weak_false_positives_by_spliceforms[number] = 0 unless weak_false_positives_by_spliceforms[number]
-  weak_false_positives_by_spliceforms[number] += 1
+def search(current_sequence)
+  gene_name = ""
+  complement = get_reverse_complement(current_sequence)
+  $truth_sequences.each_pair do |key,value|
+    reg_val = Regexp.new value
+    gene_name = key if complement.match(reg_val)
+    gene_name = key if current_sequence.match(reg_val)
+    if gene_name != ""
+      $truth_sequences.delete(key)
+      break
+    end
+  end
+  gene_name
+end
+
+#### MAIN
+
+def run(argv)
+  options = setup_options(argv)
+  setup_logger(options[:log_level])
+  $logger.debug(options)
+  $logger.debug(argv)
+  #puts options[:cut_off]
+  #read_fpkm_values(argv[0])
+
+  contig_file = ARGV[0]
+  truth_sequences_file = ARGV[1]
+  geneinfo = ARGV[2]
+
+  genes_anno = read_anno(geneinfo,options[:file_format])
+  get_truth_sequences(truth_sequences_file)
+  cut_truth_sequences(genes_anno)
+
+
+  #$expressed_isoforms = []
+  #File.open(rsem).each do |line|
+  #  next unless line =~ /^comp/
+  #  line.chomp!
+  #  info = line.split("\t")
+  #  iso_pct = info[-1].to_f
+  #  $expressed_isoforms << info[0] if iso_pct > 50.00
   #end
-when 1
-  #puts "WEAK"
-  weak_true_positive += 1
-  number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
-  #puts number
-  weak_true_positive_by_spliceform[number] = 0 unless weak_true_positive_by_spliceform[number]
-  weak_true_positive_by_spliceform[number] += 1
-when 2
-  #puts "STRONG"
-  strong_true_positive += 1
-  weak_true_positive += 1
-  number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
-  #puts number
-  weak_true_positive_by_spliceform[number] = 0 unless weak_true_positive_by_spliceform[number]
-  weak_true_positive_by_spliceform[number] += 1
-  strong_true_positives_by_spliceforms[number] = 0 unless strong_true_positives_by_spliceforms[number]
-  strong_true_positives_by_spliceforms[number] += 1
+
+
+  number_of_transcripts = `grep -c "^>" #{contig_file}`
+
+  current_sequence = ""
+  false_positive = 0
+  weak_true_positive = 0
+  strong_true_positive = 0
+  weak_true_positive_by_spliceform = []
+  #strong_true_positives_by_spliceforms = []
+  #weak_false_positives_by_spliceforms = []
+
+  number_of_all_genes = 0
+  current_comp_name = ""
+
+  File.open(contig_file).each do |line|
+
+    line.chomp!
+    if line =~ /^>/ && current_sequence != "" #&& $expressed_isoforms.include?(current_comp_name)
+      number_of_all_genes += 1
+      $logger.info "#{( number_of_all_genes.to_f / number_of_transcripts.to_f )*100} %"
+      gene_name = search(current_sequence)
+      if gene_name.empty?
+        false_positive += 1
+      else
+        weak_true_positive_by_spliceform[$number_of_spliceforms[gene_name]] = 0 unless weak_true_positive_by_spliceform[$number_of_spliceforms[gene_name]]
+        weak_true_positive_by_spliceform[$number_of_spliceforms[gene_name]] += 1
+      end
+    end
+    #  #STDERR.puts "#{( number_of_all_genes.to_f / 66 ).to_f} %"
+    #  result = search_in(current_sequence)
+    #  current_sequence = ""
+    #  case result[0]
+    #  when 0
+    #    #puts "NEGATIVE"
+    #    false_positive += 1
+    #    #if result[1] != ""
+    #    #STDERR.puts result.join(":")
+    #    if result[1] =~ /GENE/
+    #      number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
+    #    else
+    #      #STDERR.puts "fdsfdgg"
+    #      number = 0
+    #    end
+    #    #puts number
+    #    weak_false_positives_by_spliceforms[number] = 0 unless weak_false_positives_by_spliceforms[number]
+    #    weak_false_positives_by_spliceforms[number] += 1
+    #    #end
+    #  when 1
+    #    #puts "WEAK"
+    #    weak_true_positive += 1
+    #    number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
+    #    #puts number
+    #    weak_true_positive_by_spliceform[number] = 0 unless weak_true_positive_by_spliceform[number]
+    #    weak_true_positive_by_spliceform[number] += 1
+    #  when 2
+    #    #puts "STRONG"
+    #    strong_true_positive += 1
+    #    weak_true_positive += 1
+    #    number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
+    #    #puts number
+    #    weak_true_positive_by_spliceform[number] = 0 unless weak_true_positive_by_spliceform[number]
+    #    weak_true_positive_by_spliceform[number] += 1
+    #    strong_true_positives_by_spliceforms[number] = 0 unless strong_true_positives_by_spliceforms[number]
+    #    strong_true_positives_by_spliceforms[number] += 1
+    #  end
+    #end
+
+
+
+    if line =~ /^>/
+      current_comp_name = line.split(" ")[0].split(">")[1]
+      current_sequence = ""
+      next
+    end
+    current_sequence += line.upcase
+  end
+
+  #result = search_in(current_sequence)
+  #current_sequence = ""
+  #case result[0]
+  #when 0
+  #  #puts "NEGATIVE"
+  #  false_positive += 1
+  #  #if result[1] != ""
+  #  number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
+  #  #puts number
+  #  weak_false_positives_by_spliceforms[number] = 0 unless weak_false_positives_by_spliceforms[number]
+  #  weak_false_positives_by_spliceforms[number] += 1
+  #  #end
+  #when 1
+  #  #puts "WEAK"
+  #  weak_true_positive += 1
+  #  number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
+  #  #puts number
+  #  weak_true_positive_by_spliceform[number] = 0 unless weak_true_positive_by_spliceform[number]
+  #  weak_true_positive_by_spliceform[number] += 1
+  #when 2
+  #  #puts "STRONG"
+  #  strong_true_positive += 1
+  #  weak_true_positive += 1
+  #  number = (result[1].split(".")[1].to_i / 1000).floor + 1 #GENE.1802.1
+  #  #puts number
+  #  weak_true_positive_by_spliceform[number] = 0 unless weak_true_positive_by_spliceform[number]
+  #  weak_true_positive_by_spliceform[number] += 1
+  #  strong_true_positives_by_spliceforms[number] = 0 unless strong_true_positives_by_spliceforms[number]
+  #  strong_true_positives_by_spliceforms[number] += 1
+  #end
+
+  #puts false_positive
+  #puts weak_true_positive
+  #puts strong_true_positive
+
+  puts "=========="
+  puts "By number of splice forms:"
+  puts "\t\t#{(1..10).to_a.join("\t")}"
+  #puts "StroTP\t#{strong_true_positives_by_spliceforms.join("\t")}"
+  puts "WeakTP\t#{weak_true_positive_by_spliceform.join("\t")}"
+  #puts "allFP\t#{weak_false_positives_by_spliceforms.join("\t")}"
+  puts "=========="
+  #puts "Strongly True Positives\t#{strong_true_positive}"
+  #puts "Weak True Positives\t#{weak_true_positive}"
+  puts "All False Positives\t#{false_positive}"
+  puts "=========="
+  puts "All transcripts\t#{number_of_all_genes}"
+
 end
 
-puts false_positive
-puts weak_true_positive
-puts strong_true_positive
-
-puts "=========="
-puts "By number of splice forms:"
-puts "\t\t#{(1..10).to_a.join("\t")}"
-puts "StroTP\t#{strong_true_positives_by_spliceforms.join("\t")}"
-puts "WeakTP\t#{weak_true_positive_by_spliceform.join("\t")}"
-puts "allFP\t#{weak_false_positives_by_spliceforms.join("\t")}"
-puts "=========="
-puts "Strongly True Positives\t#{strong_true_positive}"
-puts "Weak True Positives\t#{weak_true_positive}"
-puts "All False Positives\t#{false_positive}"
-puts "=========="
-puts "All transcripts\t#{number_of_all_genes}"
-
-
-
+if __FILE__ == $0
+  run(ARGV)
+end
