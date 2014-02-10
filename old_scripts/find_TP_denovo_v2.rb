1af742336ec32eba961d3626b4b516960a05fe12
khayer <katharinaehayer@gmail.com>
2014-02-10 17:14:03 -0500

    got rid of regexp

--- old_scripts/find_TP_denovo_v2.rb
+++ old_scripts/find_TP_denovo_v2.rb
@@ -127,10 +127,10 @@ def cut_truth_sequences(genes_anno)
       start = transcript[1]-transcript[0]-25
       stop = seq_length-(transcript[-1]-transcript[-2]-25)
       #$logger.debug("key[-1] #{key[-1]}")
-      $truth_sequences[key[-1]] = Regexp.new pre_cut_seq[start..stop]
+      $truth_sequences[key[-1]] = pre_cut_seq[start..stop]
       $number_of_spliceforms[key[-1]] = genes_anno.number_of_spliceforms[key]
     else
-      $truth_sequences[key[-1]] = Regexp.new $truth_sequences[key[-1]]
+      $truth_sequences[key[-1]] = $truth_sequences[key[-1]]
       $number_of_spliceforms[key[-1]] = genes_anno.number_of_spliceforms[key]
     end
   end
